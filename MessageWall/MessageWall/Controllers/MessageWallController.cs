d534f27bed5b427d9f2b9be8cad99a9c34199f41
mverhelst <matthiasverhelst@hotmail.com>
2016-02-15 01:12:00 +0100

    MessageWallController with Hub: first version

---
+++ MessageWall/MessageWall/Controllers/MessageWallController.cs
@@ -0,0 +1,33 @@
+﻿using MessageWall.Hubs;
+using MessageWall.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MessageWall.Controllers
+{
+    public class MessageWallController : ApiControllerWithHub<MessageWallHub>
+    {
+        /**
+         * URL: /api/MessageWall        (POST request)
+         */
+        public string PostMessage(MessageModel message)
+        {
+            var nameEntry = "Name: " + message.UserName;
+            var messageEntry = "Message: " + message.MessageText;
+            var separator = "---------------";
+            var entry = new string[] { nameEntry, messageEntry, separator };
+
+            var filePath = "D:/message-wall.txt";
+
+            File.AppendAllLines(filePath, entry);
+
+            Hub.Clients.All.broadcastMessage(message.UserName, message.MessageText);
+
+            return "success";
+        }
+    }
+}
---
+++ MessageWall/MessageWall/Hubs/MessageWallHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageWall.Hubs
+{
+    [HubName("messageWall")]
+    public class MessageWallHub : Hub
+    {
+    }
+}
